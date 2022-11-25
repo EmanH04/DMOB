ba4b433dd2231a477b49fbbf4b2bdaa7f03d9266
EmanH04 <105911072+EmanH04@users.noreply.github.com>
2022-11-25 10:27:10 +0100

    commit rechargement

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton. = "Go roll"
     }
 }
